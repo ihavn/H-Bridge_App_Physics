9bfe72200e0162bdd85c3213acea3461851c3dd9
Ib Havn <iha@via.dk>
2020-09-28 15:44:00 +0200

    Initial

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * H-Bridge_App_Physics.c
+ *
+ * Created: 28/09/2020 15:37:14
+ * Author : IHA
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
